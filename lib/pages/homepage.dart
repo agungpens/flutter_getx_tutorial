3152c59950695a2aa28e6828dc63d2d80afd823b
agungpens <aldyryuza@it.student.pens.ac.id>
2024-02-05 17:17:15 +0700

    getx-snackbar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -16,6 +16,15 @@ class HomePages extends StatelessWidget {
                       Get.toNamed(RouteName.product) // push replacement
                     },
                 child: Text('Get All Product')),
+            ElevatedButton(
+                onPressed: () => {
+                      Get.snackbar('ini Judul', 'ini Pesan nya',
+                          icon: Icon(Icons.check_circle_outline),
+                          animationDuration: Duration(milliseconds: 200),
+                          duration: Duration(seconds: 2),
+                          backgroundColor: Colors.lightGreen),
+                    },
+                child: Text('Show Snack Bar')),
           ],
         ),
       ),
